b31615db62cbb6456e04c24ac7cc3ad4c8f8cabd
Liaoyk <liaoyk@ywsoftware.com>
2019-09-05 19:30:15 +0800

    初始化

--- server.js
+++ server.js
@@ -5,4 +5,4 @@ http.createServer(function (request,response) {
   response.end('hello world')
 }).listen('66')
 
-console.log('Server running at http://127.0.0.1:8888/');
+console.log('Server running at http://127.0.0.1:66/');
