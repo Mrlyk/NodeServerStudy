85995fac4b977feca231ec9a98a430b692def500
Liaoyk <liaoyk@ywsoftware.com>
2019-09-05 19:46:42 +0800

    初始化

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const http = require('http')
 http.createServer(function (request,response) {
   response.writeHead(200,{'Content-Type':'text/plain'})
   response.end('hello world')
-}).listen('66')
+}).listen('80')
 
-console.log('Server running at http://127.0.0.1:66/');
+console.log('Server running at http://127.0.0.1:80/');
